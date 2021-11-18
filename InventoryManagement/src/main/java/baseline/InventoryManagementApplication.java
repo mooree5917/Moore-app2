186cb04d8cc0b1edd32e71b17fd1f656d9247dbd
mooree5917 <mooree5917@Knights.ucf.edu>
2021-11-17 23:49:36 -0500

    boneworks

---
+++ InventoryManagement/src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+package baseline;
+
+public class InventoryManagementApplication {
+    public static void main(String[] args) {
+
+    }
+}
